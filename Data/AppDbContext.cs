b83158156f5c275de8912e99fc3100c4b6afefae
Vitormnz <vitorhugomnz@gmail.com>
2026-03-24 10:42:01 -0300

    Db context criado

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini_drive.Models;
+
+namespace Mini_drive.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Files> Files { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Files>()
+                .HasKey(f => f.Id);
+
+        }
+    }
+}
