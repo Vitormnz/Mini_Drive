6fa582e3e96ae7b1e04efc1a4d90773f7b920493
Vitormnz <vitorhugomnz@gmail.com>
2026-03-31 23:51:41 -0300

    add swagger e scoped for Services and repositories

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Mini_drive.Data;
+using Mini_drive.Repositories;
+using Mini_drive.Repositories.IRepositories;
+using Mini_drive.Services;
+using Mini_drive.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +13,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IFilerepository, FileRepository>();
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 // Configure the HTTP request pipeline.
@@ -20,7 +35,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.MapControllers();
 app.UseHttpsRedirection();
 
 
